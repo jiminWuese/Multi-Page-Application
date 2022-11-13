149c162ba486a265ee33a940b14bd259841ef778
Jimin Wuese <jiminwuese@gmail.com>
2022-11-13 14:38:54 +0100

    Created simple pages and initiated routing between the two pages.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route } from 'react-router-dom';
+import Welcome from './Pages/Welcome';
+import Products from './Pages/Products';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+
+      <Route path="/welcome">
+        < Welcome />
+        </Route>
+
+        <Route path="/products">
+        < Products />
+        </Route>
     </div>
   );
 }
